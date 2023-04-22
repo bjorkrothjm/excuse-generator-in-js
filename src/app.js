a7ee7856aca988dc4a7871e8d067c9e6e19f916d
bjorkrothjm <64375352+bjorkrothjm@users.noreply.github.com>
2023-04-22 21:55:10 +0000

    First commit

--- src/app.js
+++ src/app.js
@@ -2,10 +2,40 @@
 import "bootstrap";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
+/*
 window.onload = function() {
   //write your code here
   console.log("Hello Rigo from the console!");
 };
+*/
+
+window.onload = () => {
+  //document.querySelector("#excuse").innerHTML = generateExcuse();
+  let randomNumber = Math.random() * 10;
+  document.querySelector("#excuse").innerHTML = randomNumber;
+  console.log(randomNumber);
+};
+
+/*
+let generateExcuse () => {
+    const excuse_elemets = {
+      who: ["The dog", "My grandma", "His turtle", "My bird"],
+      action: ["ate", "peed", "crushed", "broke"],
+      what: ["my homework", "the keys", "the car"],
+      when: ["before the class","right on time","when I finished","during my lunch", "while I was praying"]
+    };
+
+    // creating the excuse
+    function generator(array) {
+      for (let i = 0; i < 4; i++) {
+        return array[Math.floor(Math.random() * array.lenght)];
+      };
+    };
+    
+    for (let array of excuse_elemets) {
+      var str = "";
+      str += generator(array);
+    };
+    return str;
+};
+*/
